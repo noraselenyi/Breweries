6ff1189855b34970095caf91b91471a41f726acc
Nora Selenyi <macintosh@Nora-MacBook-Air.local>
2020-02-01 17:33:36 +0100

    now gets data from given url

--- src/controllers/getbreweries.js
+++ src/controllers/getbreweries.js
@@ -4,24 +4,13 @@ const jwt = require("jsonwebtoken");
 
 const getBreweries = (req, res) => {
   const bearer = req.headers["authorization"].slice(7);
-  const result = jwt.decode(bearer);
+  const token = jwt.decode(bearer);
   
-  if (!result) { res.status(400).send('Token is not right :(')
+  if (!token) { res.status(401).send('Token is not right :(')
   } else {
     getdata()
-    res.status(200).send('holá')
+    .then((data) => res.status(200).send(data))
   }
-
-
-  // checktoken(bearer, res)
-  //   // .then(thingie => console.log(thingie))
-  //   // .then((result) => searchUsernameAndPassword(result.userdata.username, result.userdata.password))
-  //   .then(() => { res.status(200).send('okiiiiii') })
-  //   .catch((err) => {
-  //     if (err) {
-  //       res.status(400).json({ status: 'error', message: err.message });
-  //     }
-  //   });
 };
 
 module.exports = { getBreweries };
--- src/services/getData.js
+++ src/services/getData.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef */
+const fetch = require('node-fetch');
 
 const getdata = () => {
-
+  return fetch('https://api.openbrewerydb.org/breweries/search?query=dog')
+    .then(res => res.json())
 }
 
 module.exports = { getdata };
