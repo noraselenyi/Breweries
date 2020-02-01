2c2bd8566dd3416469b880ba866c1f3087c21418
Nora Selenyi <macintosh@Nora-MacBook-Air.local>
2020-02-01 18:00:35 +0100

    can now search for breweries in query

--- src/controllers/getbreweries.js
+++ src/controllers/getbreweries.js
@@ -5,11 +5,16 @@ const jwt = require("jsonwebtoken");
 const getBreweries = (req, res) => {
   const bearer = req.headers["authorization"].slice(7);
   const token = jwt.decode(bearer);
+
+  let queryparam = '';
+  if (req.query.query !== undefined) {
+    queryparam = req.query.query;
+  }
   
   if (!token) { res.status(401).send('Token is not right :(')
   } else {
-    getdata()
-    .then((data) => res.status(200).send(data))
+    getdata(queryparam)
+    .then((data) => res.status(200).json(data))
   }
 };
 
--- src/services/getData.js
+++ src/services/getData.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-undef */
 const fetch = require('node-fetch');
 
-const getdata = () => {
-  return fetch('https://api.openbrewerydb.org/breweries/search?query=dog')
-    .then(res => res.json())
+const getdata = (queryparam) => {
+  let searchFor = '';
+
+  if (queryparam !== '') {
+    searchFor = `/search?query=${queryparam}`
+  }
+  
+  return fetch(`https://api.openbrewerydb.org/breweries${searchFor}`)
+  .then(res => res.json())
 }
 
 module.exports = { getdata };
